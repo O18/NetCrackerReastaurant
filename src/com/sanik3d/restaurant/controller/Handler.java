2de0460d3b52775d9ed7b01729cf221c64ebb149
vlad <shlykovva96@yandex.ru>
2016-11-24 01:42:46 +0400

    комментарий

--- src/com/sanik3d/restaurant/controller/Handler.java
+++ src/com/sanik3d/restaurant/controller/Handler.java
@@ -4,4 +4,9 @@ package com.sanik3d.restaurant.controller;
  * Created by Александр on 24.11.2016.
  */
 public class Handler {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
 }
