fff9b30b0eb67c7351b941300c4aea9af48f627b
Александр <sanik3d>
2016-12-13 01:54:23 +0300

    LifeCycle создан

--- src/com/sanik3d/restaurant/Main.java
+++ src/com/sanik3d/restaurant/Main.java
@@ -1,24 +1,12 @@
 package com.sanik3d.restaurant;
 
-import com.sanik3d.restaurant.controller.MenuController;
-import com.sanik3d.restaurant.eventbus.EventBus;
-import com.sanik3d.restaurant.model.Menu;
-import com.sanik3d.restaurant.presenter.Presenter;
-import com.sanik3d.restaurant.view.Parser;
-import com.sanik3d.restaurant.view.View;
-
 /**
  * Created by Александр on 06.11.2016.
  */
 public class Main {
 //TODO: Maven сборка
     public static void main(String[] args) {
-        View view = new View();
-        EventBus eventBus = new EventBus();
-        Menu menu = new Menu();
-        MenuController menuController = new MenuController(menu, eventBus);
-        Presenter presenter = new Presenter(eventBus, new Parser(), view,menu);
-        view.setPresenter(presenter);
-        view.begin();
+        LifeCycle lifeCycle = new LifeCycle();
+        lifeCycle.start();
     }
 }
