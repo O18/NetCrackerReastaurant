e8ad74a4b3ff15d529e63ed29a6255bff133c2a9
vlad <shlykovva96@yandex.ru>
2017-01-11 19:47:26 +0400

    сделаны 2 окна(которые нужно доделать) и шаблон третьего, которое нужно сделать полностью, также переходы по окнам

--- src/com/sanik3d/restaurant/MenuSelectionScreen.java
+++ src/com/sanik3d/restaurant/MenuSelectionScreen.java
@@ -123,7 +123,6 @@ public class MenuSelectionScreen extends JFrame {
         }
     }
 
-
     public static void main(String[] args) {
         /*JFrame.setDefaultLookAndFeelDecorated(true);
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
